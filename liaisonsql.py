6e5416168efe8abe05554343cdf743d906637576
Nono <86175011+NonoDiscord@users.noreply.github.com>
2022-07-10 04:14:09 +0200

    Update liaisonsql.py

--- liaisonsql.py
+++ liaisonsql.py
@@ -1,66 +1,15 @@
 import sys
-import os
+from os import system
 
-import colorama
-from colorama import Fore
-from colorama import Style
+print('''
+LIAISON SQL.
+    ''')
 
-colorama.init()
+serv = input('Host: ')
+database = input('Nom DB: ')
+user = input('UserID: ')
+passwd = input('Mdp: ')
 
-def main():
-   menu()
+print(f'\nset mysql_connection_string "server={serv};database={database};userid={user};password={passwd}"\n')
 
-def menu():
-    print(Fore.YELLOW + '''
-██╗     ██╗ █████╗ ██╗███████╗ ██████╗ ███╗   ██╗    ███████╗ ██████╗ ██╗     
-██║     ██║██╔══██╗██║██╔════╝██╔═══██╗████╗  ██║    ██╔════╝██╔═══██╗██║     
-██║     ██║███████║██║███████╗██║   ██║██╔██╗ ██║    ███████╗██║   ██║██║     
-██║     ██║██╔══██║██║╚════██║██║   ██║██║╚██╗██║    ╚════██║██║▄▄ ██║██║     
-███████╗██║██║  ██║██║███████║╚██████╔╝██║ ╚████║    ███████║╚██████╔╝███████╗
-╚══════╝╚═╝╚═╝  ╚═╝╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═══╝    ╚══════╝ ╚══▀▀═╝ ╚══════╝                                                                                                                              
-                                         ''' + Style.RESET_ALL)
-    print()
-
-
-    choice = input('''
-                     [1] -> lier son SQL
-                     [2] -> GitHub
-                     [0] -> Quitter
-
-                      Entrer un numéro : ''')
-
-
-    if choice == "1":
-        print('')
-        server = input('Ip de la database: ')
-        database = input('Nom de la DB: ')
-        userid = input('Userid: ')
-        password = input('Password: ')
-        print('')
-        print('set mysql_connection_string "server={};database={};userid={};password={}"' .format(server, database, userid, password))
-        print('')
-        click = input('Cliquez sur entrée pour revenir au menu')
-        os.system('{}' .format(click))
-        os.system('cls')
-        menu()
-
-
-    elif choice == "2":
-        print('')
-        os.system('start https://github.com/NonoDiscord/liaisonsql')
-        click = input('Cliquez sur entrée pour revenir au menu')
-        os.system('{}' .format(click))
-        os.system('cls')
-        menu()
-
-    elif choice == "0":
-        sys.exit
-        print('')
-
-    else:
-        os.system('cls')
-        menu()
-    
-main()
-
-input('Cliquez sur entrée pour quitter')
+input('Quit')
